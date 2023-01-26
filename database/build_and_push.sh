5aaf900f04bd8a5564bcbb3388f390701e39a0a2
Anton_Tsyrkunou <anton.azzot@yandex.ru>
2023-01-26 16:26:33 +0300

    01-structure

--- database/build_and_push.sh
+++ database/build_and_push.sh
@@ -1,4 +1,4 @@
 docker build . -t epamantontsyrkunou/postdb
 docker push epamantontsyrkunou/postdb
-docker run -i -ti --rm --name postdb epamantontsyrkunou/postdb
+docker run -i -ti --rm -p 5432:5432 --name postdb epamantontsyrkunou/postdb
 
--- database/init.sql
+++ database/init.sql
@@ -1,7 +1,8 @@
-CREATE TABLE IF NOT EXISTS postdb (
-    id bigint,
+CREATE TABLE IF NOT EXISTS post (
+    id serial not null,
     authorId bigint,
     text varchar(255),
+    title varchar(255),
     postedAt timestamp,
     PRIMARY KEY (id)
 );
